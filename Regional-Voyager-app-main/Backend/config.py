8676373260a11a237e14f665b00815640b5b78cf
voyagerssjrtd-Ai <voyagers.sjrtd@gmail.com>
2025-11-20 14:57:20 +0530

    Delete Regional-Voyager-app-main directory

--- Regional-Voyager-app-main/Backend/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-GENAI_BASE_URL = os.getenv("AZURE_MAAS_BASE_URL", "https://genailab.tcs.in")
-GENAI_API_KEY = os.getenv("AZURE_MAAS_API_KEY", "")
-GENAI_DEFAULT_MODEL = "azure_ai/genailab-maas-DeepSeek-V3-0324"
